bbd50e665634a83fa490f19e5ab7f44ac947461b
magabrielaa <90418921+magabrielaa@users.noreply.github.com>
2022-03-13 03:21:34 -0500

    Adding code to merge vax and election datasets

--- project/data/merge.py
+++ project/data/merge.py
@@ -1,16 +1,33 @@
 import pandas as pd
-from api import cdc_county as c
-from election_data import
+import numpy as np
+from cdc_api import cdc_county as c
+
+
+# Build FIPS classifier to categorize counties
+df_fips = pd.read_csv("fips_codes.csv",dtype=str)
+fips_classifier = {}
+for _, row in df_fips.iterrows():
+    if row["County"] not in fips_classifier:
+        fips_classifier[row["County"]] = row["FIPS"]
 
 #Retrieve CDC data from API
 c.get_vax_data("cdc_data.csv")
-#Convert csv files to pandas data frame
 vax_data = pd.read_csv("cdc_data.csv")
-elect_data = pd.read_csv("")
 
-#result = pd.concat([vax_data, election_data], axis=1)
-#result = pd.concat([vax_data, election_data], axis=1, join="inner")
-
+#Retrieve election data
+elect_data = pd.read_csv("ken_test.csv")
+#Add FIPS code to facilitate merging
+elect_data["FIPS"] = elect_data["County"]
+for county in fips_classifier:
+    elect_data["FIPS"] = np.where((elect_data.FIPS == county),fips_classifier[county],elect_data.FIPS)
+  
 #Merge datasets
-df = pd.merge(vax_data, elect_data, on=["County", "key2"])
-df.to_csv('merged_data.csv')
+# Generates 4444 rows
+df_2 = pd.merge(vax_data, elect_data, how='inner', on = "FIPS")
+# Generates 2961 rows
+df_3 = pd.merge(vax_data, elect_data, how='left', on = "FIPS")
+
+#Convert to csv
+df_2.to_csv('merged_data.csv')
+df_3.to_csv('merged_data_duplicates.csv')
+    
