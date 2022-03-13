c92cbf3ba58f10b20c88ebec29b334fc16f444a2
magabrielaa <90418921+magabrielaa@users.noreply.github.com>
2022-03-13 04:22:03 -0500

    Cleaned up code

--- project/data/merge.py
+++ project/data/merge.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 from cdc_api import cdc_county as c
 
-
 # Build FIPS classifier to categorize counties
 df_fips = pd.read_csv("fips_codes.csv",dtype=str)
 fips_classifier = {}
@@ -19,20 +18,19 @@ elect_data = pd.read_csv("ken_test.csv")
 #Add FIPS code to facilitate merging
 elect_data["FIPS"] = elect_data["County"]
 for fips, (state, county) in fips_classifier.items():
-    #elect_data["FIPS"] = np.where((elect_data.State == state) & (elect_data.County == county), fips, elect_data.FIPS)
-    elect_data["FIPS"] = np.where((elect_data["State"] == state) & (elect_data["County"] == county), fips, elect_data["FIPS"])
+    elect_data["FIPS"] = np.where((elect_data["State"] == state) & (elect_data["County"] == county), fips, 
+    elect_data["FIPS"])
 elect_data = elect_data.drop(columns=["State", "County"])
 
-
 #Merge datasets
-# Generates 2942 rows
+# Inner Merge
 df_1 = pd.merge(vax_data, elect_data, how='inner', on = "FIPS")
 df_1 = df_1.rename(columns={"State_x":"State","County_x":"County"})
-# Generates 3200 rows
+# Left Merge
 df_2 = pd.merge(vax_data, elect_data, how='left', on = "FIPS")
 df_2 = df_2.rename(columns={"State_x":"State","County_x":"County"})
 
-#Convert to csv
+#Convert merged df's to csv
 df_1.to_csv('inner_merged.csv')
 df_2.to_csv('left_merged.csv')
     
