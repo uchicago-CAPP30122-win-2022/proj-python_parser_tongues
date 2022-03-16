471daa8d847dd69b471d52c825b2eba39750c796
Ken Kliesner <kenkliesner@uchicago.edu>
2022-03-16 14:41:24 -0500

    updating 2020_election_results.csv code

--- project/data/merge.py
+++ project/data/merge.py
@@ -14,7 +14,8 @@ c.get_vax_data("cdc_data.csv")
 vax_data = pd.read_csv("cdc_data.csv", dtype=str)
 
 #Retrieve election data
-elect_data = pd.read_csv("election_results.csv",dtype=str)
+elect_data = pd.read_csv("2020_election_results.csv",dtype=str)
+
 #Add FIPS code to facilitate merging
 elect_data["FIPS"] = elect_data["County"]
 for fips, (state, county) in fips_classifier.items():
