22ff820276bfd5d378e76f6658a58d9d85bf5056
magabrielaa <90418921+magabrielaa@users.noreply.github.com>
2022-03-16 16:43:10 -0500

    Fixed bug

--- project/install.sh
+++ project/install.sh
@@ -15,27 +15,5 @@ echo -e "Install is complete."
 
 python3 data/election_crawler/crawler.py
 
-echo -e "3. Running Crawler..."
-
-python3 data/merge.py 
-
-echo -e "4. Getting Data from CDC and merging with Control/Election Data.. "
-
-cp data/data.csv regression
-
-python3 regression/regression.py
-
-echo -e "5. Running Regression.. "
-
-
-
-python3 visual_exp/map_dem_rep.py
-
-
-echo -e "6. Creating Map.. "
-
-
-
-
 
 deactivate 
