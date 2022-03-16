88aae3136d84b216fcdc2bc9dfbe442c0aa92fe8
Ken Kliesner <kenkliesner@uchicago.edu>
2022-03-16 13:43:48 -0500

    uploading changes to ken file, but then will delete

--- project/data/election_crawler/ken_crawler.py
+++ project/data/election_crawler/ken_crawler.py
@@ -1,124 +1,146 @@
-############### Ken's Implementation ##############
+'''
+CAPP30122 W'22: Final Project - Election Site Crawler
 
+Author: Ken Kliesner
+3/16/2022
+'''
 
 import csv
-import pandas as pd
 import requests
 import bs4
 
 
-dem_url = ("https://uselectionatlas.org/RESULTS/party.php?year=2020&type=national&no=1&f=1&off=0&elect=0")
-rep_url = ("https://uselectionatlas.org/RESULTS/party.php?year=2020&type=national&no=2&f=1&off=0&elect=0")
-############### Ken's Implementation ##############
-
-# Come back to this commented out one later to add complexity
-#GEN_ELECT_RESULTS = "https://uselectionatlas.org/RESULTS/national.php?year=2020&f=1&off=0&elect=0"
-GEN_ELECT = "https://uselectionatlas.org/RESULTS/data.php?year=2020&datatype=national&def=1&f=1&off=0&elect=0"
+GENERAL_ELECTION = "https://uselectionatlas.org/RESULTS/data.php?year= \
+                    2020&datatype=national&def=1&f=1&off=0&elect=0"
 DOMAIN_URL = "https://uselectionatlas.org/RESULTS/"
 
 
-def go(desired_filename="election_results.csv", url=GEN_ELECT):
+def scrape(desired_filename="election_results.csv"):
     '''
-    Placeholder Docstring
+    Run a report from scraping across all of the 2020 US Election Data from
+    uselectionatlas.org and grab all of the results per state and per county
+    to then be output in a CSV file.
+
+    Input:
+      desired_filename (str): a string containing the filename the user would
+        like their csv to be saved as (must end in .csv). This is set with a
+        default of "election_results.csv" if the user does not provide their
+        own filename.
+    
+    Output:
+      CSV file saved to the current directory, with name input to function.
     '''
-    state_urls = find_state_urls(url)
+    assert desired_filename.endswith(".csv")
+
+    state_urls = find_state_urls(GENERAL_ELECTION)
     election_results = get_election_results(state_urls)
     write_csv(election_results, desired_filename)
 
 
-##### MAIN SCRAPING FUNCTIONS #####
-
-# Function to get each individual state's URL
+#################### AUXILIARY FUNCTIONS ####################
 def find_state_urls(url):
     '''
-    Scrapes 2020 US Presidential Election data for a political party.
+    Scrapes 2020 US Presidential Election full-country data map to find the
+    urls for the pages of each individual state map.
 
     Input:
-        url (str): url to scrape (GEN_ELECT: full US 2020 US Election Results)
+      url (str): url to scrape (GEN_ELECT: full 2020 US Election Results)
+    
     Output:
       state_urls (dict): a dictionary mapping state keys to a url of their
-        election results (states are full written-out words).
+        election results (states are full written-out words, not abbreviation).
     '''
-
-    # USE GEN_ELECT GLOBAL VARIABLE AS STARTING URL
     state_urls = {}
     soup = get_soup(url)
     if soup:
-        all_state_data = soup.find_all("div")[1].form.table.tbody.tr.td.table.tbody.find_all("tr")
+        all_state_data = soup.find_all("div")[1].form.table.tbody.tr.td. \
+                         table.tbody.find_all("tr")
         for state_data in all_state_data:
             state = state_data.find_all("td", class_="name")[0].a.text
-            each_state_url = state_data.find_all("td", class_="name")[0].a["href"]
-            state_urls[state] = DOMAIN_URL + each_state_url
+            each_st_url = state_data.find_all("td", class_="name")[0].a["href"]
+            state_urls[state] = "".join((DOMAIN_URL, each_st_url))
 
     return state_urls
 
 
-# Main function to process URL
 def get_election_results(state_urls):
     '''
+    Main function to process each state's URL. It will take the state's URL and
+    parse through each of the state's counties to find each candidate's results
+    in percentage form.
 
     Input:
-      state_urls (dict): a dictionary mapping state keys to a url of their election results. 
+      state_urls (dict): a dictionary mapping state keys to a url of their
+        election results (states are full written-out words, not abbreviation).
 
     Output:
-      all_election_results (dict): a dictionary mapping each state to another
-        dictionary of its counties, which are then mapped to candidates and
-        their election results. 
-
-    Example Output: 
-      {
-      'State1': 
-        {'County1': {'Biden': '19.5%', 'Trump': '78.6%', 'Other': '1.7%'}, 
-         'County2': {'Biden': '19.5%', 'Trump': '78.6%', 'Other': '1.7%'}}
-      'Pennsylvania': 
-        {'Teton': {'Biden': '67.1%', 'Trump': '29.6%', 'Other': '2.7%'},
-         'Uinta': {'Biden': '16.9%', 'Trump': '79.7%', 'Other': '2.5%'}}
-      }
+      all_election_results (dict): a dictionary of nested dictionaries mapping
+        each state to another dictionary of its counties, which are then mapped
+        to a final dictionary of candidate keys mapped to their election
+        results (in percentage form).
     '''
-    all_election_results = {}  # ALL OF THE US ELECTION RESULTS BY STATE
+    all_election_results = {}
     for state, state_url in state_urls.items():
         state_gen_elections_soup = get_soup(state_url)
         if state_gen_elections_soup:
-            state_gen_elections_link = DOMAIN_URL + state_gen_elections_soup.find_all("div")[1].div.find_all("a")[1]["href"]
+            state_gen_elect = state_gen_elections_soup.find_all("div")[1]. \
+                              div.find_all("a")[1]["href"]
+            state_gen_elections_link = "".join((DOMAIN_URL, state_gen_elect))
             all_state_county_data_soup = get_soup(state_gen_elections_link)
             if all_state_county_data_soup:
-                all_state_county_data_link = DOMAIN_URL + all_state_county_data_soup.find_all("div")[1].find_all("table")[0].find_all("table")[6].find_all("li")[1].a["href"]
-                individual_county_data_soup = get_soup(all_state_county_data_link)
+                st_cnty_data = all_state_county_data_soup.find_all("div")[1]. \
+                               find_all("table")[0].find_all("table")[6]. \
+                               find_all("li")[1].a["href"]
+                all_st_cnty_data_link = "".join((DOMAIN_URL, st_cnty_data))
+                individual_county_data_soup = get_soup(all_st_cnty_data_link)
                 if individual_county_data_soup:
-                    counties = individual_county_data_soup.div.div.find_all("table")
-                    state_results = {}  # ALL OF AN INDIVIDUAL STATE'S RESULTS BY COUNTY
+                    counties = individual_county_data_soup.div. \
+                               div.find_all("table")
+                    state_results = {}
                     for county in counties:
                         county_name = county.b.text
-                        county_results = get_county_data(county)                      
+                        county_results = get_county_data(county)
                         state_results[county_name] = county_results
                     all_election_results[state] = state_results
-                    
+
     return all_election_results
 
 
 def get_county_data(county):
     '''
+    A helper function for get_election_results(), which goes down to the level
+    of each candidate and extracts the necessary data, adding each candidate
+    key mapped with their results to the output dictionary.
+
+    Input:
+      county ("table" tag): an HTML "table" tag for a given state that contains
+        the election results for that county.
+
+    Output:
+      county_results (dict): a dictionary of candidate keys mapped to their
+        election results (in percentage form).
     '''
-    county_results = {}  # ALL OF AN INDIVIDUAL COUNTY'S RESULTS BY CANDIDATE/PERCENTAGE
-    default_perc = 0.0
+    county_results = {}
+    pierce_perc = 0.0
     for i, result in enumerate(county.tbody.find_all("tr")):
-        if i == 0:  # Biden, need bc of how page is written
+        if i == 0:  # Only for Biden, need bc of how programmer wrote HTML page
             cand_name = result.find_all("td", class_="cnd")[i].text
-            cand_perc = float(result.find_all("td", class_="per")[i].text.strip("%"))
-        else:  # All other candidates
+            cand_perc = float(result.find_all("td", class_="per")[i]. \
+                              text.strip("%"))
+        else:  # For all other candidates
             cand_name = result.td.text
-            cand_perc = float(result.find_all("td", class_="per")[0].text.strip("%"))
+            cand_perc = float(result.find_all("td", class_="per")[0]. \
+                              text.strip("%"))
         
         if cand_name == "Pierce":
-            default_perc = cand_perc
+            pierce_perc = cand_perc
         elif cand_name == "Other":
-            county_results[cand_name] = round(cand_perc + default_perc, 1)
+            county_results[cand_name] = round(cand_perc + pierce_perc, 1)
         else:
             county_results[cand_name] = cand_perc
-   
-    default_perc = 0.0
+        
     if "Other" not in county_results:
-        county_results["Other"] = default_perc
+        county_results["Other"] = pierce_perc
     
     county_winner = max(county_results, key=county_results.get)
     if county_winner == "Biden":
@@ -129,16 +151,16 @@ def get_county_data(county):
     return county_results
 
 
-# Helper just to get Soup
 def get_soup(url):
     '''
-    Opens a connection to the specified URL, reads data and makes soup.
+    A helper function to find the bs4 soup object. It opens a connection to the
+    specified URL, reads the data, and returns the needed soup.
 
-    Inputs:
-        (str): URL
+    Input:
+        url (str): the string of a specified URL.
 
-    Outputs:
-        (soup object)/ None: Soup or None if connection failed
+    Output:
+        soup (soup object): the HTML soup object or None if connection failed.
     '''
     r = requests.get(url)
     if r.status_code == 404 or r.status_code == 403:
@@ -148,45 +170,31 @@ def get_soup(url):
         soup = bs4.BeautifulSoup(s, "html5lib")
         return soup
 
+
 def write_csv(election_dict, output_filename):
     '''
-    Creates CSV file with the name provided to the function and with contents
-    of the course identifier word indices.
+    Creates a CSV file in the current directory with the name provided to the
+    scape() function. Contents will include each state, county, candidate
+    results, and whether or not the winner of that county was a democrat (this
+    is for purposes later in our color-coded map visualization).
 
-    Puts together scraped 2020 US Presidential Election data with Democrat and
-    Republican results into a csv file.
-
-
-    Inputs:
-        index_filename: the name for the CSV of the index.
-        index_dict: dictionary mapping course identifiers to words
-
-    Outputs:
-        No outputs except for the creation of a new csv file with the 
-        index_filename provided that is saved within the current directory.
     Input:
-        (str): csv file name 
-        (str): Democrat results url
-        (str): Republican results url
+      election_dict (dict): a dictionary of nested dictionaries mapping each
+        state to another dictionary of its counties, which are then mapped to a
+        final dictionary of candidate keys mapped to their election results (in
+        percentage form).
+      output_filename (str): the name for the CSV to be created.
 
-
-    Example Dict Input:
-
-    {'Rhode Island': {'Bristol': {'Biden': 63.5, 'Trump': 34.3, 'Other': 2.2},      
-    'Kent': {'Biden': 52.8, 'Trump': 45.1, 'Other': 2.1},
-    'Newport': {'Biden': 63.9, 'Trump': 34.1, 'Other': 2.0},
-    'Providence': {'Biden': 60.5, 'Trump': 37.6, 'Other': 1.9},
-    'Washington': {'Biden': 58.6, 'Trump': 39.2, 'Other': 2.2}},
-    'Delaware': {'Kent': {'Biden': 51.2, 'Trump': 47.1, 'Other': 1.7},
-    'New Castle': {'Biden': 67.8, 'Trump': 30.7, 'Other': 1.5},
-    'Sussex': {'Biden': 43.8, 'Trump': 55.1, 'Other': 1.1}}}
+    Output:
+      CSV file saved to the current directory, with name input to function.
     '''
     with open(output_filename, "w", newline="") as csvfile:
-        #elec_data = csv.DictWriter(csvfile, delimiter = ",")
         elec_data = csv.writer(csvfile, delimiter = ",")
-        elec_data.writerow(["State", "County", "Biden Vote %", "Trump Vote %", "Other Vote %", "Was Winner Democrat?"])
-
+        elec_data.writerow(["State", "County", "Biden Vote %", "Trump Vote %", 
+                            "Other Vote %", "Was Winner Democrat?"])
         for state, counties in election_dict.items():
             for county, results in counties.items():                
                 elec_data = csv.writer(csvfile, delimiter=",")
-                elec_data.writerow([state, county, results["Biden"], results["Trump"], results["Other"], results["Winner Dem?"]])
+                elec_data.writerow([state, county, results["Biden"], 
+                                    results["Trump"], results["Other"], 
+                                    results["Winner Dem?"]])
