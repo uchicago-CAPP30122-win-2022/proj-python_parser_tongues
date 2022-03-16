dc1e4bb2329a391bc2b80ac86536dbbf73ae8603
Stefan Manuel <stefmanuel@cs.uchicago.edu>
2022-03-16 12:56:59 -0500

    Added full regression results

--- project/regression/regression.py
+++ project/regression/regression.py
@@ -1,10 +1,8 @@
-from distutils.command.clean import clean
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
 import numpy as np
 import pandas as pd
-import sqlite3 as sql 
 import statsmodels.api as sm
+
 
 
 '''
@@ -21,23 +19,21 @@ def regress(regression_cols, dependent_col, filename="data/data.csv"):
 
     
     cleaned_data = all_data.dropna()
-    enc = OneHotEncoder()
     svi_dummies = pd.get_dummies(cleaned_data['SVI'])
-    # cleaned_data = cleaned_data.transform(np.log )
     cleaned_data = pd.concat([cleaned_data, svi_dummies], axis=1)
     model = LinearRegression(fit_intercept = False).fit(cleaned_data[regression_cols], cleaned_data[dependent_col])
-    # model = sm.WLS(cleaned_data[dependent_col], cleaned_data[regression_cols], weights=cleaned_data['Population']).fit()
-    # model = sm.GLS(cleaned_data[dependent_col], cleaned_data[regression_cols]).fit()
+    # model1 = sm.WLS(cleaned_data[dependent_col], cleaned_data[regression_cols], weights=cleaned_data['Population']).fit()
+    model1 = sm.GLS(cleaned_data[dependent_col], cleaned_data[regression_cols]).fit()
     rv = model.coef_
     rv = pd.DataFrame(data = rv, columns = regression_cols)
-    # print(model.summary())
 
-    return rv
+    return model1.summary(),rv
+
 
 regression_cols = [ 'Was Winner Democrat?', 'UNEMP', 'INCOME', 'Population', "Metro Status", 'Population 65 Plus', 'A', 'B', 'C', 'D' ]
-dependent_col = ['Complete 18 Plus Percent']
+dependent_col = ['Completeness Percent']
 data = "data/data.csv"
-print(regress(regression_cols, dependent_col, data))
+#print(regress(regression_cols, dependent_col, data))
 
 
 
