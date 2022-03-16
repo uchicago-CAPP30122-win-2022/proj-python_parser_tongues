75eb0b3b29241bdff84266458ecdb319dbb2edc9
Stefan Manuel <stefmanuel@cs.uchicago.edu>
2022-03-16 12:35:41 -0500

    Minor Changes to results

--- project/regression/regression.py
+++ project/regression/regression.py
@@ -3,6 +3,7 @@ from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
 import numpy as np
 import pandas as pd
 import sqlite3 as sql 
+import statsmodels.api as sm
 
 
 '''
@@ -23,11 +24,14 @@ def regress(regression_cols, dependent_col, filename="data/data.csv"):
     svi_dummies = pd.get_dummies(cleaned_data['SVI'])
 
     cleaned_data = pd.concat([cleaned_data, svi_dummies], axis=1)
-    model = LinearRegression().fit(cleaned_data[regression_cols], cleaned_data[dependent_col])
+    model = LinearRegression(fit_intercept = False).fit(cleaned_data[regression_cols], cleaned_data[dependent_col])
+    # model = sm.WLS(cleaned_data[dependent_col], cleaned_data[regression_cols], weights=cleaned_data['Population']).fit()
+    # model = sm.GLS(cleaned_data[dependent_col], cleaned_data[regression_cols]).fit()
     rv = model.coef_
     rv = pd.DataFrame(data = rv, columns = regression_cols)
+    # print(model.summary())
 
-    return model.intercept_, rv
+    return rv
 
 regression_cols = [ 'Was Winner Democrat?', 'UNEMP', 'INCOME', 'Population', "Metro Status", 'Population 65 Plus', 'A', 'B', 'C', 'D' ]
 dependent_col = ['Completeness Percent']
